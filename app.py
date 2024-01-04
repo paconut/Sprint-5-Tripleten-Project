59d0b46bad781aac7b36b87f7405af89a6654f4e
paconut <paconut1@gmail.com>
2024-01-04 14:58:30 -0600

    cambiamos los botones por checkboxes

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import pandas as pd
 import plotly.express as px
 
 car_data = pd.read_csv('../vehicles_us.csv')
-hist_button = st.button('Construir histograma')
 st.header('App')
+hist_button = st.checkbox('Construir histograma')
 if hist_button:
     st.write('Crea histograma.')
-    fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    fig1 = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig1, use_container_width=True)
+
+disp_button = st.checkbox('Construir gráfico de dispersión')
+if disp_button:
+    st.write('Crea gráfico de dispersión.')
+    fig2 = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig2, use_container_width=True)
